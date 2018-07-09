8c5c68c544aa26639d97191fc91d5a7c978297ca
sbachr <sbachr0501@gmail.com>
2018-07-09 23:42:44 +0700

    revisi

--- 5_number_in_word.js
+++ 5_number_in_word.js
@@ -1,6 +1,6 @@
 function number_word(number) {
     // Your code here
-    var words = ['','satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh','delapan','sembilan', 'sepuluh', 'sebelas' ];
+    const words = ['','satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh','delapan','sembilan', 'sepuluh', 'sebelas' ];
     let index = 0
    
     if(number >= 0 && number <= 11){
@@ -8,47 +8,47 @@ function number_word(number) {
     }
   
     if(number>11 && number<20){
-      let index = number-10;
+       index = number-10;
       return `${words[index]}belas`;
     }
     
     if(number >= 20 && number < 100){
-      let index = Math.floor(number%10) ;
+       index = Math.floor(number%10) ;
       return `${words[index]} puluh ${number_word(number%10)}` ;
     }
     
     if(number >= 100 && number < 200){
-      let index = Math.floor(number-100);
+       index = Math.floor(number-100);
       return `seratus ${number_word(index)}`;
     }
    
     if(number >= 200 && number < 1000){
-      let index = Math.floor(number/100);
+       index = Math.floor(number/100);
       return `${words[index]} ratus ${number_word(number%100)}`
     }
     
     if(number >= 1000 && number < 2000){
-      let index = Math.floor(number-1000);
+       index = Math.floor(number-1000);
       return `seribu ${number_word(index)}`;
     }
     
     if(number >= 2000 && number < 1000000){
-      let index = Math.floor(number/1000);
+       index = Math.floor(number/1000);
       return `${number_word(index)} ribu ${number_word(number%1000)}`
     }
     
     if(number >= 1000000 && number < 1000000000){
-      let index = Math.floor(number/1000000);
+       index = Math.floor(number/1000000);
       return `${number_word(index)} juta ${number_word(number%1000000)}`
     }
     
     if(number >= 1000000000 && number < 1000000000000){
-      let index = Math.floor(number/1000000000);
+       index = Math.floor(number/1000000000);
       return `${number_word(index)} miliyar ${number_word(number%1000000000)}`
     }
     
     if(number >= 1000000000000 && number < 1000000000000000){
-      let index = Math.floor(number/1000000000000);
+       index = Math.floor(number/1000000000000);
       return `${number_word(index)} triliun  ${number_word(number%1000000000000)}`
     }
     
